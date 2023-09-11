6fa3f174f19848c00427808b607f33cf577c3e80
fade697 <132776309+fade697@users.noreply.github.com>
2023-09-11 14:35:10 +0300

    tikets

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -1,41 +1,47 @@
-const Ticket = require('../models/ticket-model');
-
-
-
+const Ticket = require('../models/ticket_module');
 
 // Create a new ticket
 const createTicket = async (req, res) => {
-    //todo
+        //todo
 }
-
+ 
 
 // Update a ticket by ID
-const updateTicketById = async (req, res) => {
+const updateTicket = async (req, res) => {
         //todo
 }
 
-// Pick up Ticket by ID
-const getTicketById = async (req, res) => {
+// Pick up a ticket by ID
+const pickUpTicket = async (req, res) => {
+        //todo
+}
+
+// Pick down a ticket by ID
+const pickDownTicket = async (req, res) => {
         //todo
 }
 
 // Delete a ticket by ID
-const deleteTicketById = async (req, res) => {
+const deleteTicket = async (req, res) => {
         //todo
 }
 
 // Get a list of all tickets
-const getAllTickets = async (req, res) => {
-    //todo
+const readTickets = async (req, res) => {
+        //todo
 }
 
-
-
+// Get a ticket by ID
+const readTicketById = async (req, res) => {
+        //todo
+}
 
 module.exports = {
   createTicket,
-  getAllTickets,
-  getTicketById,
-  updateTicketById,
-  deleteTicketById,
+  updateTicket,
+  pickUpTicket,
+  pickDownTicket,
+  deleteTicket,
+  readTickets,
+  readTicketById,
 };
